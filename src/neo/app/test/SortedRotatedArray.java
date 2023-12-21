95ebce5795ddc853f27e7feac0422477802af7a0
FarinPatel <farin.patel@osos.om>
2023-12-21 11:49:52 +0530

    Rotated array program added

--- src/neo/app/test/SortedRotatedArray.java
+++ src/neo/app/test/SortedRotatedArray.java
@@ -10,8 +10,10 @@ public class SortedRotatedArray {
         List<Integer> arr = Arrays.asList(3,4,5,1,2);
         List<Integer> arr2 = Arrays.asList(4,5,6,7,0,1,2);
         Collections.sort(arr);
+        System.out.println("Original Array1 ::"+arr);
         System.out.println("Array1 ::"+arr.get(0));
         Collections.sort(arr2);
+        System.out.println("Original Array2 ::"+arr2);
         System.out.println("Array2 ::"+arr2.get(0));
     }
 }
